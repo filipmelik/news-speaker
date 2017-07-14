6f06c5509f1b90750d2f0b053cb108bda9c12f9a
Filip Melik <filip.melik@heureka.cz>
2017-07-14 10:49:56 +0200

    Initial Commit

---
+++ speaker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  speaker
+//
+//  Created by Filip Melik on 14/07/2017.
+//  Copyright © 2017 Filip Melik. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
